eabed6a138b2c6f84a46da27dcb3702689beccfa
ale150375 <ale150375@hotmail.com>
2022-10-31 20:37:20 -0300

    my avocados page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,62 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+const baseUrl = 'https://platzi-avo.vercel.app';
+
+const appNode = document.querySelector('#app');
+
+//internacionalizacion Intl
+//1.- formato a fechas
+//2.- format monedas
+
+const formatPrice = (price) => {
+   const newPrice = new window.Intl.NumberFormat('en-EN', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(price);
+    
+    return newPrice
+}
+
+//web api
+//conectarnos al server
+window
+    .fetch(`${baseUrl}/api/avo`)
+//procesar la rta, y convertirla en JSON
+    .then((respuesta) => respuesta.json())
+//JSON -> Data -> renderizar info browser
+    .then(responseJson => {
+
+        const todosLosItems = [];
+        
+        responseJson.data.forEach((item) => {
+        
+            //crear img
+            const imagen = document.createElement('img');
+            imagen.src = `${baseUrl}${item.image}`;
+            imagen.className = 'h-16 w-16 md:h-24 rounded-full mx-auto md:mx-0 md:mr-6'
+        
+            //crear titulo
+            const title = document.createElement('h2');
+            title.textContent = item.name;
+            //1.-Agregando estilos utilizando las propiedades de estilos
+            //title.style = 'font-size: 2rem';
+            //title.style.fontSize = '3rem';
+            //2.-Agregando estilos Utilizando clases
+            //title.className = 'muy-grande';
+            title.className = 'text-xl text-red-700';
+            
+            
+            //crear precio
+            const price = document.createElement('div');
+            price.textContent = formatPrice(item.price);
+
+            const container = document.createElement('div');
+            container.append(imagen, title, price);
+            container.className = 'contenedor';
+            
+            todosLosItems.push(container);
+        });
+        appNode.append(...todosLosItems)
+    })
+    //Si queremos mejorar el codigo podemos utilizar promise->async/await
